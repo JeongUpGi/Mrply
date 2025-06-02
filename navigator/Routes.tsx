8447318859457973972f64b22e7ed43ef55faa84
JeongUpGi <dkdfl1201@gmail.com>
2025-06-02 11:35:09 +0900

    바텀탭 - 스택 구조로 변경

--- navigator/Routes.tsx
+++ navigator/Routes.tsx
@@ -1,14 +1,66 @@
 import React from 'react';
 import {Image} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import homeScreen from '../screen/homeScreen';
-import newMusicScreen from '../screen/newMusicScreen';
-import searchScreen from '../screen/searchScreen';
-import storageScreen from '../screen/storageScreen';
-import allMenuScreen from '../screen/allMenuScreen';
+import HomeScreen from '../screen/home/homeScreen.tsx';
+import NewMusicScreen from '../screen/newMusic/newMusicScreen';
+import SearchScreen from '../screen/search/searchScreen';
+import StorageScreen from '../screen/storage/storageScreen';
+import AllMenuScreen from '../screen/allMenu/allMenuScreen';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="homeScreen"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="homeScreen" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function NewMusicStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="newMusicScreen"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="newMusicScreen" component={NewMusicScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function SearchStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="searchScreen"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="searchScreen" component={SearchScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function StorageStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="storageScreen"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="storageScreen" component={StorageScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function MenuStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="allMenuScreen"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="allMenuScreen" component={AllMenuScreen} />
+    </Stack.Navigator>
+  );
+}
 
 function BottomTabs() {
   return (
@@ -19,73 +71,68 @@ function BottomTabs() {
         tabBarInactiveTintColor: '#8e8e93',
       }}>
       <Tab.Screen
-        name="homeScreen"
-        component={homeScreen}
+        name="홈"
+        component={HomeStack}
         options={{
-          // tabBarShowLabel: false,
-          tabBarLabel: '홈',
-          tabBarIcon: () => (
+          tabBarIcon: ({color, size}) => (
             <Image
               source={require('../asset/images/home.png')}
-              style={{width: 24, height: 24}}
+              style={{width: size, height: size, tintColor: color}}
             />
           ),
+          tabBarLabel: '홈',
         }}
       />
       <Tab.Screen
-        name="newMusicScreen"
-        component={newMusicScreen}
+        name="새로운 음악"
+        component={NewMusicStack}
         options={{
-          // tabBarShowLabel: false,
-          tabBarLabel: '새로운 음악',
-          tabBarIcon: () => (
+          tabBarIcon: ({color, size}) => (
             <Image
               source={require('../asset/images/music_storage.png')}
-              style={{width: 24, height: 24}}
+              style={{width: size, height: size, tintColor: color}}
             />
           ),
+          tabBarLabel: '새로운 음악',
         }}
       />
       <Tab.Screen
-        name="searchScreen"
-        component={searchScreen}
+        name="검색"
+        component={SearchStack}
         options={{
-          // tabBarShowLabel: false,
-          tabBarLabel: '검색',
-          tabBarIcon: () => (
+          tabBarIcon: ({color, size}) => (
             <Image
               source={require('../asset/images/search.png')}
-              style={{width: 24, height: 24}}
+              style={{width: size, height: size, tintColor: color}}
             />
           ),
+          tabBarLabel: '검색',
         }}
       />
       <Tab.Screen
-        name="storageScreen"
-        component={storageScreen}
+        name="보관함"
+        component={StorageStack}
         options={{
-          // tabBarShowLabel: false,
-          tabBarLabel: '보관함',
-          tabBarIcon: () => (
+          tabBarIcon: ({color, size}) => (
             <Image
               source={require('../asset/images/electric_lightbulb.png')}
-              style={{width: 24, height: 24}}
+              style={{width: size, height: size, tintColor: color}}
             />
           ),
+          tabBarLabel: '보관함',
         }}
       />
       <Tab.Screen
-        name="allMenuScreen"
-        component={allMenuScreen}
+        name="전체 메뉴"
+        component={MenuStack}
         options={{
-          // tabBarShowLabel: false,
-          tabBarLabel: '전체 메뉴',
-          tabBarIcon: () => (
+          tabBarIcon: ({color, size}) => (
             <Image
               source={require('../asset/images/all_menu.png')}
-              style={{width: 24, height: 24}}
+              style={{width: size, height: size, tintColor: color}}
             />
           ),
+          tabBarLabel: '전체 메뉴',
         }}
       />
     </Tab.Navigator>
