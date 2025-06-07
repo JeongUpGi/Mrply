cab614063ab537c8f79d0a0c536866c6a942aaa7
JeongUpGi <dkdfl1201@gmail.com>
2025-06-07 13:26:10 +0900

    오디오 url(mp4), data 가져오는 네트워크 함수 정의 및 모델 정의

--- model/model.ts
+++ model/model.ts
@@ -39,3 +39,16 @@ export interface RecentSearchItems {
   onPress: (item: string) => void; // 항목 클릭 시 호출될 함수
   onDelete: (item: string) => void; // 삭제 버튼 클릭 시 호출될 함수
 }
+
+export interface AudioPlaybackData {
+  url: string;
+  title: string;
+  artist: string;
+  artwork: string;
+  id: string;
+}
+
+// 추후 변수 추가 될 수 있기에 모델로 정의
+export interface AudioServiceResponseData {
+  audioPlaybackData: AudioPlaybackData;
+}
--- network/network.ts
+++ network/network.ts
@@ -1,7 +1,13 @@
 import {YOUTUBE_API_KEY} from '@env';
-import {SearchResponse} from '../model/model';
+import {
+  SearchResponse,
+  SearchResultMusicItem,
+  AudioPlaybackData,
+  AudioServiceResponseData,
+} from '../model/model';
 
 const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3';
+const BASE_URL = 'http://172.30.1.93'; //url 추후 수정
 
 /**
  * YouTube 동영상 음악을 검색하는 함수
@@ -31,5 +37,59 @@ export const searchVideos = async (
   } catch (error) {
     console.error('Error fetching YouTube search results:', error);
     throw error;
+  }
+};
+
+export const getAudioUrlAndData = async (
+  searchResultMusicItem: SearchResultMusicItem,
+): Promise<AudioServiceResponseData> => {
+  try {
+    const url =
+      `${BASE_URL}:3000/api/get-youtube-audio?videoId=` +
+      searchResultMusicItem.id.videoId;
+    const response = await fetch(url);
+    console.log('Backend response status:', response.status);
+
+    if (!response.ok) {
+      const errorBody = await response.text();
+      throw new Error(
+        `Backend responded with status ${response.status}: ${errorBody}`,
+      );
+    }
+
+    const audioData = await response.json();
+    console.log('Received audio data from backend:', audioData);
+
+    if (!audioData.audioUrl) {
+      throw new Error('Backend response missing audioUrl.');
+    }
+
+    const audioPlaybackData: AudioPlaybackData = {
+      url: audioData.audioUrl,
+      title:
+        audioData.title ||
+        searchResultMusicItem.snippet.title ||
+        'Unknown Title',
+      artist:
+        audioData.author ||
+        searchResultMusicItem.snippet.channelTitle ||
+        'Unknown Artist',
+      artwork:
+        audioData.thumbnailUrl ||
+        searchResultMusicItem.snippet.thumbnails.medium.url ||
+        'fallback_image_url',
+      id: searchResultMusicItem.id.videoId!,
+    };
+
+    return {
+      audioPlaybackData: audioPlaybackData,
+    };
+  } catch (err: unknown) {
+    console.error('Error in audioService:', err);
+    if (err instanceof Error) {
+      throw new Error('Failed to get audio URL: ' + err.message);
+    } else {
+      throw new Error('Unknown error occurred in audioService.');
+    }
   }
 };
