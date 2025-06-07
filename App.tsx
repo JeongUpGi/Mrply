3d6dc9550530eec5854d794a638f95ebec3a8a76
JeongUpGi <dkdfl1201@gmail.com>
2025-06-07 13:29:09 +0900

    앱 최초 업 로드시 track-player 초기 세팅 및 설정

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux'; // react-redux의 Provider 임포트
 import {PersistGate} from 'redux-persist/integration/react'; // redux-persist의 PersistGate 임포트
 import {store, persistor} from './store';
@@ -6,8 +6,37 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import {View, ActivityIndicator} from 'react-native';
 import BottomTabs from './navigator/Routes';
+import TrackPlayer from 'react-native-track-player';
+
+// 앱 최초 업로드 시 TrackPlayer 초기 설정
+async function setupPlayer() {
+  let isSetup = false;
+  try {
+    await TrackPlayer.setupPlayer();
+    isSetup = true;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      if (error.message.includes('Already have a player instance')) {
+        isSetup = true;
+      } else {
+        console.error('Failed to setup player', error.message);
+      }
+    } else {
+      console.error(
+        'Failed to setup player with unexpected error type:',
+        error,
+      );
+    }
+  } finally {
+    return isSetup;
+  }
+}
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    setupPlayer();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate
